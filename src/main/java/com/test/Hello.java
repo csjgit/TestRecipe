f035fc18b7be397619839bd60011c6f6c78159d0
csjgit <shekhar204@gmail.com>
2025-03-04 09:16:57 +0530

    Test declartive

---
+++ src/main/java/com/test/Hello.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import org.openrewrite.config.Environment;
+
+public class Hello {
+    public static void main(String[] args) {
+
+
+      /*  Environment env = Environment.builder()
+                .load(rewriteYmlLoader) // can be called more than once for multiple files
+                .scanClasspath(classpath) // classpath scans for META-INF/rewrite/*.yml
+                .scanUserHome() // looks for `~/.rewrite/rewrite.yml
+                .build();*/
+    }
+}
